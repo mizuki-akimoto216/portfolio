3d5ed56a737c9bb3465d83243208cd49431eb4fb
Mizuki Akimoto <Mizuki.Akimoto@vanarts.com>
2023-07-07 11:40:18 -0700

    strapi setup

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 3306),
--- server/config/env/production/database.js
+++ server/config/env/production/database.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
       host: env('DATABASE_HOST', '167.172.100.229'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'aDmin@123a'),
+      user: env('DATABASE_USERNAME', 'strapiadminweb23'),
+      password: env('DATABASE_PASSWORD', 'aDministrator@321b'),
       ssl: false
     },
     debug: false,
